d6217baca7e2472c4e798c6233dd3b06786039e2
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-04 04:42:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if find_by_name(name) == nil
-      create(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
     else
-      find_by_name(name)
+      self.find_by_name(name)
     end
   end
 end
